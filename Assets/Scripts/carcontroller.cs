4ceab5ef8cef7a4294c0d638ae3cf9feb068f269
kimsangminhhh <80864787+kimsangminhhh@users.noreply.github.com>
2021-03-25 10:05:48 +0900

    스와프카 완성

---
+++ Assets/Scripts/carcontroller.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class carcontroller : MonoBehaviour
+{
+    float speed = 0.0f;
+    Vector2 startPos;
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            startPos = Input.mousePosition;
+        }
+        else if (Input.GetMouseButtonUp(0))
+        {
+            Vector2 endPos = Input.mousePosition;
+            float swipelength = (endPos.x - startPos.x);
+            this.speed = swipelength / 500.0f;
+
+            GetComponent<AudioSource>().Play();
+        }
+        transform.Translate(this.speed, 0, 0);
+        this.speed *= 0.98f;
+
+    }
+}
